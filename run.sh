39ed46f43408694868b962203b33190fc4069e19
MHS <muhamad.salman2001@gmail.com>
2025-07-30 17:45:26 +0200

    fix: updated action versions

--- run.sh
+++ run.sh
@@ -7,15 +7,22 @@ sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
+# Create golang directory if it doesn't exist
+mkdir -p golang
+
 protoc --go_out=./golang --go_opt=paths=source_relative \
  --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
 
-cd golang/${SERVICE_NAME}
-go mod init github.com/MHS-20/Order-Proto/golang/${SERVICE_NAME} ||true
-go mod tidy
-
-cd ../../
+# Check if the service directory was created
+if [ -d "golang/${SERVICE_NAME}" ]; then
+    cd golang/${SERVICE_NAME}
+    go mod init github.com/MHS-20/Order-Proto/golang/${SERVICE_NAME} ||true
+    go mod tidy
+    cd ../../
+else
+    echo "Warning: golang/${SERVICE_NAME} directory not created. Skipping Go module initialization."
+fi
 git config --global user.email "muhamad.salman2001@gmail.com"
 git config --global user.name "MHS-20"
 
